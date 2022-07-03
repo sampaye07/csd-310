f81e5cb4ae276ad39c1a983605105473d1c59ec1
sampaye07 <83557944+sampaye07@users.noreply.github.com>
2022-07-03 16:24:15 -0400

    Create pytech_insert.py

---
+++ module_5/pytech_insert.py
@@ -0,0 +1,82 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Student{
+	"student_id": "14080822",
+	"first_name": "Samuel",
+	"last_name": "Paye",
+	"CYBR": [
+		{
+			"Enrollment": {
+				"term": "Winter",
+				"gpa": "3.9",
+				"start_date": "20221201",
+				"end_date": "20230301",
+				"Course" {
+					"course_id": "CYBR1000",
+					"description": "Hacking Crypto",
+					"instructer": "Professor Blackhat",
+					"grade": "A"
+				},
+				{...},
+				{...}
+			},
+			{...},
+			{...}
+		}
+	]
+}
