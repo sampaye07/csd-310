b75a94cd370461e0c3a749531696a694e9d3ab23
sampaye07 <83557944+sampaye07@users.noreply.github.com>
2022-07-03 16:54:59 -0400

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,82 +1,117 @@
+""" 
+    Title: mongodb_test.py
+    Author: Sam Paye
+    Date: 03 July 2022
+    Description: Test program for connecting to a 
+                 MongoDB Atlas cluster
+"""
 
+""" import statements """
+from pymongo import MongoClient
 
+# MongoDB connection string 
+url = mongodb+srv://admin:<password>@cluster0.kcxahui.mongodb.net/?retryWrites=true&w=majority
 
+# connect to the MongoDB cluster 
+client = MongoClient(url)
 
+# connect pytech database
+db = client.pytech
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Student{
-	"student_id": "14080822",
-	"first_name": "Samuel",
-	"last_name": "Paye",
-	"CYBR": [
-		{
-			"Enrollment": {
-				"term": "Winter",
-				"gpa": "3.9",
-				"start_date": "20221201",
-				"end_date": "20230301",
-				"Course" {
-					"course_id": "CYBR1000",
-					"description": "Hacking Crypto",
-					"instructer": "Professor Blackhat",
-					"grade": "A"
-				},
-				{...},
-				{...}
-			},
-			{...},
-			{...}
-		}
-	]
+""" New student documtents
+# Thorin Oakenshield
+Thorin = {
+    "student_id": "1007",
+    "first_name": "Thorin",
+    "last_name": "Oakenshield",
+    "enrollments": [
+        {
+            "term": "Winter",
+            "gpa": "3.9",
+            "start_date": "20221201",
+            "end_date": "20230301",
+            "courses": [
+                {
+                    "course_id": "CYBR1000",
+                    "description": "Hacking Crypto",
+                    "instructor": "Professor Blackhat",
+                    "grade": "A"
+                },
+                {...},
+		{...}
+            ]
+        }
+    ]
+    [...],
+    [...]
 }
+
+# Bilbo Baggins
+Bilbo = {
+    "student_id": "1008",
+    "first_name": "Bilbo",
+    "last_name": "Baggins",
+    "enrollments": [
+        {
+            "term": "Winter",
+            "gpa": "4.0",
+            "start_date": "20221201",
+            "end_date": "20230301",
+            "courses": [
+                {
+                    "course_id": "CYBR1000",
+                    "description": "Hacking Crypto",
+                    "instructor": "Professor Blackhat",
+                    "grade": "A+"
+                },
+                {...},
+		{...}
+            ]
+        }
+    ]
+    [...],
+    [...]
+}
+
+#Frodo Baggins
+Frodo = {
+    "student_id": "1009",
+    "first_name": "Frodo",
+    "last_name": "Baggins",
+    "enrollments": [
+        {
+            "term": "Winter",
+            "gpa": "2.3",
+            "start_date": "20221201",
+            "end_date": "20230301",
+            "courses": [
+                {
+                    "course_id": "CYBR1000",
+                    "description": "Hacking Crypto",
+                    "instructor": "Professor Blackhat",
+                    "grade": "D-"
+                },
+                {...},
+		{...}
+            ]
+        }
+    ]
+    [...],
+    [...]
+}
+
+# get students collection
+students = db.students
+
+#insert statements with output
+print("\n  -- INSERT STATEMENTS --")
+thorin_student_id = students.insert_one(Thorin).inserted_id
+print(thorin_student_id)
+
+bilbo_student_id = students.insert_one(Bilbo).inserted_id
+print(bilbo_student_id)
+
+frodo_student_id = students.insert_one(Frodo).inserted_id
+print(frodo_student_id)
+
+input("\n\n  End of program, press any key to exit... ")
