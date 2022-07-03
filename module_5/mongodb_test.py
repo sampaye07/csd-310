858436e0bc89c560dae6e3a31adfda070f7bb1db
sampaye07 <83557944+sampaye07@users.noreply.github.com>
2022-07-03 16:09:15 -0400

    Update mongodb_test.py
    To be honest, I was a little confused with this assignment. I was able to do everything other than get 'students' in the output. I connected MongoDB to VS Code, but the issue I ran into was making the application connection. I retrieved the following: mongodb+srv://admin:<password>@cluster0.kcxahui.mongodb.net/?retryWrites=true&w=majority, and I changed <password> to admin. I was still unable to get the 'students' output. If you could explain what I'm missing, I would be greatly appreciative.

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,1 +1,26 @@
+""" 
+    Title: mongodb_test.py
+    Author: Sam Paye
+    Date: 03 July 2022
+    Description: Test program for connecting to a 
+                 MongoDB Atlas cluster
+"""
 
+""" import statements """
+from pymongo import MongoClient
+
+# MongoDB connection string 
+url = mongodb+srv://admin:<password>@cluster0.kcxahui.mongodb.net/?retryWrites=true&w=majority
+
+# connect to the MongoDB cluster 
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech
+
+# show the connected collections 
+print("\n -- Pytech Collection List --")
+print(db.list_collection_names())
+
+# show an exit message
+input("\n\n  End of program, press any key to exit... ")
